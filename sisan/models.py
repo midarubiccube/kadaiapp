eedb3f719d863d8d2bb9c85f2e4904c0bca03e07
daimiya <mi.dai.tomo@gmail.com>
2025-07-24 13:41:04 +0900

    add　index

--- sisan/models.py
+++ sisan/models.py
@@ -4,11 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Sisan(models.Model):
-    user = models.ForeignKey(User, verbose_name="ユーザー",on_delete=models.CASCADE, null=True,blank=True)
+    user = models.ForeignKey(User, verbose_name="管理者",on_delete=models.CASCADE, null=True,blank=True)
     name = models.CharField(max_length=100, verbose_name='資産名')
     sisan_id = models.CharField(max_length=100, verbose_name='資産管理番号')
     name = models.CharField(max_length = 100, verbose_name='型式番号')
     buy_date = models.DateField(verbose_name='取得年月日')
     remove_date = models.DateField(verbose_name='除却年月日', null=True, blank=True)
     price = models.IntegerField(verbose_name='取得価格')
-    place = models.CharField(max_length=100, verbose_name='保管場所')    
+    place = models.CharField(max_length=100, verbose_name='保管場所')
+
